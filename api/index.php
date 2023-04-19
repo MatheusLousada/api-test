f74866e0c1007ca41943bcd2e8854258b20d0234
Matheus Carvalho Lousada <matheusclousada@gmail.com>
2023-04-19 01:15:32 -0300

    info

--- api/index.php
+++ api/index.php
@@ -1,11 +1,14 @@
 <?php
 
+phpinfo();
+
 // header("Access-Control-Allow-Origin", "*");
 // header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 // header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization");
 
 require_once("vendor/autoload.php");
 ini_set('display_errors', 'off');
+
 use App\Core\Router;
 
 $router = new Router();
