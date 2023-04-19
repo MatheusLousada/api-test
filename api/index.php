c2ceeaf3812ef006b4a1ac91b7e7d02932112a06
Matheus Carvalho Lousada <matheusclousada@gmail.com>
2023-04-19 01:17:17 -0300

    inf

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 
-phpinfo();
-
 // header("Access-Control-Allow-Origin", "*");
 // header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 // header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization");
