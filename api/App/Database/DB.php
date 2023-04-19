5feaa6edf306282df04b5f9c8220a7dd494d47ea
Matheus Carvalho Lousada <matheusclousada@gmail.com>
2023-04-19 11:09:02 -0300

    teste db

--- api/App/Database/DB.php
+++ api/App/Database/DB.php
@@ -8,9 +8,13 @@ class DB
 
     public function __construct()
     {
+        echo json_encode(["error" => 'veio aqui']);
         $this->conn = new \mysqli("containers-us-west-20.railway.app", "root", "railway", "7090");
         if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
+            // die("Connection failed: " . $this->conn->connect_error);
+            http_response_code(500);
+            echo json_encode(["error" => 'Connection failed']);
+            exit;
         }
     }
 
