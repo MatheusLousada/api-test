7eaa128437aefa1f21ced1ccf0e3e04f862d339b
Matheus Carvalho Lousada <matheusclousada@gmail.com>
2023-04-19 20:17:08 -0300

    db

--- api/App/Core/Model.php
+++ api/App/Core/Model.php
@@ -12,19 +12,14 @@ class Model
     {
         if (!isset(self::$connection)) {
 
+            $db = new DB();
 
             http_response_code(200);
             echo json_encode(["login" => 'aqui indentro']);
             exit;
 
-            $db = new DB();
             self::$connection = $db->getConnection();
         }
-
-
-        http_response_code(200);
-        echo json_encode(["login" => 'aqui infora']);
-        exit;
 
         return self::$connection;
     }
