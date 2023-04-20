b5eac13ef436b3cb843ae0a4c4397cd7e09d992d
Matheus Carvalho Lousada <matheusclousada@gmail.com>
2023-04-20 00:19:43 -0300

    db

--- api/App/Database/DB.php
+++ api/App/Database/DB.php
@@ -11,10 +11,11 @@ class DB
         $servername = "containers-us-west-20.railway.app";
         $username = "root";
         $password = "edbNCXlqmZ8NXQGqYgps";
+        $database = "railway";
         $port = 7090;
         $protocol = "TCP";
 
-        $this->conn = mysqli_connect($servername, $username, $password, "", $port, $protocol);
+        $this->conn = mysqli_connect($servername, $username, $password, $database, $port, $protocol);
         if (!$this->conn) {
             http_response_code(500);
             echo json_encode(["error" => 'Connection failed: ' . mysqli_connect_error()]);
